8981af0289608726af2251a755996bf0502e52fd
Shashank Kumar Pandey <iamskp99@gmail.com>
2021-09-07 21:00:51 +0530

    Update

--- Qualification Round/B.cpp
+++ Qualification Round/B.cpp
@@ -40,9 +40,122 @@ string to_binary(int x) {
 }
 
 ///////////////////////////////////////////////////////////////////
+int n;
+vector<string> mat;
+unordered_map<int, int> graph;
 
 void solve(int now) {
-
+	cin >> n;
+	string s;
+	int ans, i, j, cnt, flag, e1, e2, rom, cnt1;
+	i = 0;
+	while (i < n) {
+		cin >> s;
+		mat.pb(s);
+		i = i + 1;
+	}
+	ans = 1000000000000000000;
+	i = 0;
+	while (i < n) {
+		j = 0;
+		cnt = 0;
+		flag = 0;
+		while (j < n) {
+			if (mat[i][j] == 'O') {
+				flag = 1;
+				break;
+			}
+			if (mat[i][j] == '.') {
+				cnt += 1;
+			}
+			j = j + 1;
+		}
+		if (flag == 0) {
+			ans = min(ans, cnt);
+			graph[cnt] += 1;
+		}
+		i = i + 1;
+	}
+	// cout << ans << " " << graph[ans] << nline;
+	i = 0;
+	while (i < n) {
+		j = 0;
+		cnt = 0;
+		flag = 0;
+		while (j < n) {
+			if (mat[j][i] == 'O') {
+				flag = 1;
+				break;
+			}
+			if (mat[j][i] == '.') {
+				cnt += 1;
+			}
+			j = j + 1;
+		}
+		if (flag == 0) {
+			ans = min(ans, cnt);
+			graph[cnt] += 1;
+		}
+		i = i + 1;
+	}
+	if (ans == 1) {
+		i = 0;
+		while (i < n) {
+			j = 0;
+			while (j < n) {
+				if (mat[i][j] == '.') {
+					rom = 0;
+					e1 = 0;
+					cnt1 = 0;
+					flag = 0;
+					while (e1 < n) {
+						if (mat[i][e1] == 'O') {
+							flag = 1;
+							break;
+						}
+						if (mat[i][e1] == '.') {
+							cnt1 += 1;
+						}
+						e1 = e1 + 1;
+					}
+					if (flag == 0) {
+						if (cnt1 == 1) {
+							rom += 1;
+						}
+					}
+					// rom = 0;
+					e1 = 0;
+					cnt1 = 0;
+					flag = 0;
+					while (e1 < n) {
+						if (mat[e1][j] == 'O') {
+							flag = 1;
+							break;
+						}
+						if (mat[e1][j] == '.') {
+							cnt1 += 1;
+						}
+						e1 = e1 + 1;
+					}
+					if (flag == 0) {
+						if (cnt1 == 1) {
+							rom += 1;
+						}
+					}
+					if (rom == 2) {
+						graph[ans] -= 1;
+					}
+				}
+				j = j + 1;
+			}
+			i = i + 1;
+		}
+	}
+	if (ans == 1000000000000000000) {
+		cout << "Case #" << now + 1 << ": " << "Impossible" << nline;
+		return;
+	}
+	cout << "Case #" << now + 1 << ": " << ans << " " << graph[ans] << nline;
 }
 
 int32_t main() {
